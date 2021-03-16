7dc60bd1a38f1f2e7e13f097791c6bb7b9ec1ad4
Issacyau <74083539+Issacyau@users.noreply.github.com>
2021-03-16 17:13:11 +0000

    Update jy_algorithm.py

--- jy_algorithm.py
+++ jy_algorithm.py
@@ -1,24 +1,27 @@
 ### Implementing Jiang&Yin's algorithm
-def alg_1(Ns):
+def alg_2(SI):
+    Ns = len(SI)
     index_string_list = []
+    sis = []
     for i in range(1,Ns+1):
         index_string_list.append(i)
-    sis = index_string_list
+        sis.append(SI[i-1])
     i = Ns
     j = i + 1
-    si = sis.index(Ns)
-    Nsis = len(sis)
+    si = index_string_list.index(Ns-1)
+    Nsis = len(index_string_list)
     loop =1
     while i != j:
-        inter_sis = list(zip(sis, sis[1:] + sis[:0]))
+        inter_sis = list(zip(index_string_list, index_string_list[1:] + index_string_list[:0]))
         if j > (Ns):
             j = 1
         elif (i,j) not in inter_sis and (j,i) not in inter_sis:
-            sis.append(j)
+            index_string_list.append(j)
+            sis.append(SI[j-1])
             Nsis += 1
             i = j
             j = i + 1
         else:
             j = j+1
-    print(sis,i,j,si,Nsis)
+    print(index_string_list,i,j,si,Nsis)
     return sis
